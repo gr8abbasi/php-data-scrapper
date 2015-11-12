88e7431d2f173448aa5ebdf8b51c7f6d24c7d393
Muhammad Kashif Abbasi <gr8.abbasi@gmail.com>
2015-11-12 18:15:23 +0100

    First commit with basic structure

---
+++ autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+spl_autoload_register(
+    function ($class) {
+        static $classes = null;
+        if ($classes === null) {
+            $classes = array(
+                'Sorting\\AbstractSorting'      => '/src/Sorting/AbstractSorting->php',
+                'Sorting\\QuickSort\\QuickSort' => '/src/Sorting/QuickSort/QuickSort->php',
+                'Sorting\\MergeSort\\MergeSort' => '/src/Sorting/MergeSort/MergeSort->php',
+
+            );
+        }
+        $cn = strtolower($class);
+        if (isset($classes[$class])) {
+            require __DIR__ . $classes[$class];
+        }
+
+    },
+        true,
+        false
+
+    );
